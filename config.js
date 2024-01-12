8536f71692de47ae06dc44413ea5a5edf62a9d71
Chami113 <146918870+Chami113@users.noreply.github.com>
2024-01-12 16:14:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -15,16 +15,16 @@ global.devs = '94761516805';
 global.website = 'https://github.com/Sithuwa/SITHUWA-MD' //wa.me/+94000000000000
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/15b1dd8aeaa47888d75d7.jpg'
 module.exports = {
-  sessionName: process.env.SESSION_ID || "",      //Put Your Session Id Here
+  sessionName: process.env.SESSION_ID || "SITHUWA-MD;;;eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoic0NmcWtNdkh1OW5adnIzaGdlUkxhSWhiUEhaek5JS3VKcmQ2SjFXQ08zYz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiYVhicHVxUW9XNFpGZ2E0N0NHdXVZVXpuTG9TQ2krTHF4a1pvUy8vMTMwWT0ifX0sInNpZ25lZElkZW50aXR5S2V5Ijp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJHS2xmaFhaZS9KNDUzWFRFU2JtWFk5QlVBbjB4MHhXK2JwR05iVmV4bW5ZPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ2dHkvUkgzaW9zcXNucytMak92MWlLQWloTitqU081OTVVclhrUlh5K0J3PSJ9fSwic2lnbmVkUHJlS2V5Ijp7ImtleVBhaXIiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjRDbExySkltVy9pbVVrV3ZYL0NlVHJEMHQvRU5nbU1PUmJLWTNxT2paWFU9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkpKYmZlMlp5cDFULzlpRThZenZQcWQ1ZTlpamJVK3JXZEd4TXpwcW1Ia3M9In19LCJzaWduYXR1cmUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCbzg3OEdaOXJuVlptUy9Pam5tanNKVG5RTVljSXlJYnJJUDZXbU1PK1FsVktqd3VReDgzOTR4dENUVjhaaTRKRFpuMkJZOWhqTW9lMDRjNU1VNStDQT09In0sImtleUlkIjoxfSwicmVnaXN0cmF0aW9uSWQiOjc2LCJhZHZTZWNyZXRLZXkiOiJpRXR1ZmM2azVJMCs2bUZTRCtSa215dU1aVlM5U3d5enVQRlUzS1BxMHVvPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJGcTJSb2hlQlNuS19Pem1zSHE4TUVRIiwicGhvbmVJZCI6IjZiNWQxYjU1LWM4ZTQtNGM0NS1hZmM3LWQwMzVkNTRiNjFiMCIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ0ekJFRElkSGovdUh5ZWJsbGlPanFnT0ptMTg9In0sInJlZ2lzdGVyZWQiOmZhbHNlLCJiYWNrdXBUb2tlbiI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImVxdkt5bzlzMEg1bDZjTXFXMnZUQzNXYVdzYz0ifSwicmVnaXN0cmF0aW9uIjp7fSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ0tua3hORUdFS3lzaEswR0dBRT0iLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoiL1hEeXJiVHdWSmtrc2ZUeDJyQ0JTTjNmQUNYTlAyVlgrSlZ3MGdPY1ZsMD0iLCJhY2NvdW50U2lnbmF0dXJlIjoiTjZDbkczM3lydnJ5THI2RkVpcDFYaG1rQzVpaWFFWUlxcHNiWFRvcnArQUQ1YXhVTG1oSXRuaDBEZURsTU82TmpNSHN5THN3SkozSjQraFJxdVlSQUE9PSIsImRldmljZVNpZ25hdHVyZSI6ImhxMk00NlRUd2xaQWtKQzlxZk82ZVRkTG5mRGI5WUVCTS9tVm5pWk5VaFUwN3pHQWZnNy95ZjRNTk53WmRCYXV5MlROR01KWmdXd1NoSmlBZmZEOURnPT0ifSwibWUiOnsiaWQiOiI5NDc3NTA4OTQzOTo0MkBzLndoYXRzYXBwLm5ldCIsIm5hbWUiOiLigKIg8J2YvvCdmYPwnZi88J2ZiPCdmYQgLSDwnZi98J2ZgPCdmLzwnZmP8J2ZlSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiI5NDc3NTA4OTQzOTo0MkBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJmMXc4cTIwOEZTWkpMSDA4ZHF3Z1VqZDN3QWx6VDlsVi9pVmNOSURuRlpkIn19XSwicGxhdGZvcm0iOiJzbWJhIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzA1MDU1NzkzfQ==",      //Put Your Session Id Here
   author:  process.env.PACK_AUTHER ||  'SITHUWA BOT',
   packname:  process.env.PACK_NAME || 'MADE BY SITHUM KALHARA',
   
   botname:   process.env.BOT_NAME === undefined ? "SITHUWA-MD" : process.env.BOT_NAME,
   ownername: process.env.OWNER_NAME === undefined ? 'sithumkalhara' : process.env.OWNER_NAME,  
-  auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
-  autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
+  auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? true : process.env.AUTO_READ_STATUS,
+  autoreaction:  process.env.AUTO_REACTION  === undefined ? true : process.env.AUTO_REACTION ,
   antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
-  alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
+  alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? true : process.env.ALWAYS_ONLINE,
   antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '234' : process.env.FAKE_COUNTRY_CODE,
   readmessage:  process.env.READ_MESSAGE === undefined ? false : process.env.READ_MESSAGE,
   auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? false : process.env.AUTO_STATUS_SAVER,
